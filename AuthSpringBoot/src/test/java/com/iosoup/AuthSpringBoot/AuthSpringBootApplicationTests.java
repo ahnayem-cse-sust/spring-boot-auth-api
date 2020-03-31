c03dd5fe6d8ba8ba3b2c2466b526d6d3d3b67d7c
Nayem <ahnayem.sust@gmail.com>
2020-03-31 13:13:05 +0600

    project int

---
+++ AuthSpringBoot/src/test/java/com/iosoup/AuthSpringBoot/AuthSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.iosoup.AuthSpringBoot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuthSpringBootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
