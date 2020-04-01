59920127ea98db3b36f2ba04c93e9034591c8cb1
Nayem <ahnayem.sust@gmail.com>
2020-04-01 20:53:05 +0600

    dir

--- springboot-jwt/src/main/java/com/iosoup/springbootjwt/SecurityConfigurer.java
+++
@@ -1,8 +0,0 @@
-package com.iosoup.springbootjwt;
-
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-
-public class SecurityConfigurer extends WebSecurityConfigurerAdapter {
-	@Override
-	protected
-}
