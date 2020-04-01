481a156b32bff574a770f5644a2f57ab01c348b6
Nayem <ahnayem.sust@gmail.com>
2020-04-01 20:04:04 +0600

    new init

---
+++ AuthApiJwt/src/test/java/com/iosoup/auth_api_jwt/AuthApiJwtApplicationTests.java
@@ -0,0 +1,13 @@
+package com.iosoup.auth_api_jwt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuthApiJwtApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
