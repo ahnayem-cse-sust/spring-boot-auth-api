9b803a439d11b3c0aeefcc3851ec3e584bc71145
Nayem <ahnayem.sust@gmail.com>
2020-04-04 23:36:22 +0600

    war file deleted

--- AuthApi/src/main/java/com/iosoup/auth_api/HelloController.java
+++ AuthApi/src/main/java/com/iosoup/auth_api/HelloController.java
@@ -1,29 +1,29 @@
-///*
-// * To change this license header, choose License Headers in Project Properties.
-// * To change this template file, choose Tools | Templates
-// * and open the template in the editor.
-// */
-//package com.iosoup.auth_api;
-//
-//import org.springframework.boot.SpringApplication;
-//import org.springframework.boot.autoconfigure.SpringBootApplication;
-//import org.springframework.web.bind.annotation.RequestMapping;
-//import org.springframework.web.bind.annotation.RestController;
-//
-///**
-// *
-// * @author nayem
-// */
-//
-//@SpringBootApplication
-//@RestController
-//public class HelloController {
-//    public static void main(String[] args) {
-//        SpringApplication.run(HelloController.class, args);
-//    }
-//    
-//    @RequestMapping(value = "/")
-//    public String hello(){
-//        return "Hello World";
-//    }
-//}
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.iosoup.auth_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ *
+ * @author nayem
+ */
+
+@SpringBootApplication
+@RestController
+public class HelloController {
+    public static void main(String[] args) {
+        SpringApplication.run(HelloController.class, args);
+    }
+    
+    @RequestMapping(value = "/")
+    public String hello(){
+        return "Hello World";
+    }
+}
