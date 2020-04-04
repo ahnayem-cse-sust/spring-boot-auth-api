3d81f502ae3798a12690a83f9e4a3433db095aff
Nayem <ahnayem.sust@gmail.com>
2020-04-05 00:54:49 +0600

    application file

--- AuthApi/src/main/java/com/iosoup/auth_api/HelloController.java
+++ AuthApi/src/main/java/com/iosoup/auth_api/HelloController.java
@@ -5,7 +5,6 @@
  */
 package com.iosoup.auth_api;
 
-import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -18,12 +17,9 @@ import org.springframework.web.bind.annotation.RestController;
 @SpringBootApplication
 @RestController
 public class HelloController {
-    public static void main(String[] args) {
-        SpringApplication.run(HelloController.class, args);
-    }
     
     @RequestMapping(value = "/")
     public String hello(){
-        return "Hello World";
+        return "Hello World Application!!";
     }
 }
