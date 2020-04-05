61b38b5e69fb4d6bacb3f4569e5bbab494e12f4e
Nayem <ahnayem.sust@gmail.com>
2020-04-05 23:03:34 +0600

    person service

---
+++ AuthApi/src/main/java/com/iosoup/auth_api/dao/FakePersonDataAccessService.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.iosoup.auth_api.dao;
+
+import com.iosoup.auth_api.model.Person;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.UUID;
+
+/**
+ *
+ * @author nayem
+ */
+public class FakePersonDataAccessService implements PersonDao{
+
+    private static List<Person> DB = new ArrayList<>();
+    
+    @Override
+    public int insertPerson(UUID id, Person person) {
+        DB.add(new Person(id, person.getName()));
+        return 1;
+    }
+    
+}
