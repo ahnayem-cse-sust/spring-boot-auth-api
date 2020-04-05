692b5d4a86dfcf78f2f07daa448fe041376b3c77
Nayem <ahnayem.sust@gmail.com>
2020-04-05 22:41:40 +0600

    person dao

---
+++ AuthApi/src/main/java/com/iosoup/auth_api/dao/PersonDao.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.iosoup.auth_api.dao;
+
+import com.iosoup.auth_api.model.Person;
+import java.util.UUID;
+
+/**
+ *
+ * @author nayem
+ */
+public interface PersonDao {
+    
+    int insertPerson(UUID id, Person person);
+    
+    default int addPerson(Person person){
+        UUID id = UUID.randomUUID();
+        return insertPerson(id, person);
+    }
+}
